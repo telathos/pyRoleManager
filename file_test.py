d012799e55e141b90004daa0f295e725958a4c8b
Tym Rehm <tymrehm@hotmail.com>
2017-11-11 11:57:11 -0500

    test

--- file_test.py
+++
@@ -1,9 +0,0 @@
-cfg_dir="cfg"
-with open(cfg_dir+"/sttchart.csv") as f:
-    statchart =f.readlines()
-f.close()
-
-#statchart= statchart.strip("\n")
-for x in statchart:
-    print "line:",x
-#print statchart
